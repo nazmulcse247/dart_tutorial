d7aa6c970fb47d53567bd3ece7e798bea92566dd
nazmulcse247 <nazmulhoque247@gmail.comn>
2025-12-21 17:16:55 +0600

    add: loop

---
+++ bin/06_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  for(int i = 1; i<=10; i++) {
+    print(i);
+  }
+
+  //Foreach loop
+  List<String> list = ["Apple", "Banana", "Orange"];
+  for (var name in list) {
+    print(name);
+  }
+}
