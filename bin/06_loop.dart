4899dc3af187c87e451f2f5e975848edf504c57c
nazmulcse247 <nazmulhoque247@gmail.comn>
2025-12-22 11:04:13 +0600

    add: loop nested loop

--- bin/06_loop.dart
+++ bin/06_loop.dart
@@ -8,4 +8,27 @@ void main() {
   for (var name in list) {
     print(name);
   }
+
+  //while loop
+  int j = 1;
+  while(j <= 10) {
+    print(j);
+    j++;
+  }
+
+  //do while loop
+  int k = 1;
+  do {
+    if(k % 2 == 0) {
+      print(k);
+    }
+    k++;
+  } while(k <= 10);
+
+  //Nested loop
+  for(int m = 1; m <= 3; m++) {
+    for(int n = 1; n <= 2; n++) {
+      print("m = $m, n = $n");
+    }
+  }
 }
