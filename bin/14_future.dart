108b1bb75e1326fd1c21a0fa4db4b10c0ee6a3e4
nazmulcse247 <nazmulhoque247@gmail.comn>
2026-02-07 17:20:52 +0600

    add stream

--- bin/14_future.dart
+++ bin/14_future.dart
@@ -1,14 +1,27 @@
-Future<String> createOrderMessage() async {
-  var order = await fetchUserOrder();
-  return 'Your order is: $order';
+// synchronous operation: A synchronous operation blocks other operations from executing until it completes.
+// synchronous function: A synchronous function only performs synchronous operations.
+// asynchronous operation: Once initiated, an asynchronous operation allows other operations to execute before it completes.
+// asynchronous function: An asynchronous function performs at least one asynchronous operation and can also perform synchronous operations
+
+Future<void> printOrderMessage() async {
+  try {
+    print('Awaiting user order...');
+    var order = await fetchUserOrder();
+    print(order);
+  } catch (err) {
+    print('Caught error: $err');
+  }
 }
 
-Future<String> fetchUserOrder() =>
-    // Imagine that this function is
-    // more complex and slow.
-    Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
+Future<String> fetchUserOrder() {
+  // Imagine that this function is more complex.
+  var str = Future.delayed(
+    const Duration(seconds: 4),
+    () => throw 'Cannot locate user order',
+  );
+  return str;
+}
 
-Future<void> main() async {
-  print('Fetching user order...');
-  print(await createOrderMessage());
+void main() async {
+  await printOrderMessage();
 }
---
+++ bin/15_streams.dart
@@ -0,0 +1,21 @@
+
+//In Dart, a Stream represents a sequence of asynchronous events. Think of it as data that arrives over time, not all at once—the async counterpart of an Iterable
+Future<int> sumStream(Stream<int> stream) async {
+  var sum = 0;
+  await for (final value in stream) {
+    sum += value;
+  }
+  return sum;
+}
+
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= to; i++) {
+    yield i;
+  }
+}
+
+void main() async {
+  var stream = countStream(10);
+  var sum = await sumStream(stream);
+  print(sum);
+}
